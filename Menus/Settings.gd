79de1b8617979de26d7bcf5bcd8b820a15117199
TristanLe07 <107384818+TristanLe07@users.noreply.github.com>
2022-11-07 13:29:13 +0800

    e

--- Menus/Settings.gd
+++ Menus/Settings.gd
@@ -1,6 +1,15 @@
 extends Control
 
-
+var music_bus = AudioServer.get_bus_index("Music")
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Menus/Controls.tscn")
+
+
+func _on_HSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(music_bus,true)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(music_bus,true)
+	else:
+		AudioServer.set_bus_mute(music_bus,false)
